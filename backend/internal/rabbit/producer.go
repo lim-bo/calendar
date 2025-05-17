d4b129b04b75aed138ff846c01917ebc6b812fca
lim-bo <artemshap66@gmail.com>
2025-05-17 19:57:26 +0300

    feat: added delayed message proccessing

--- backend/internal/rabbit/producer.go
+++ backend/internal/rabbit/producer.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"time"
 
+	"github.com/bytedance/sonic"
+	"github.com/lim-bo/calendar/backend/models"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -37,9 +39,27 @@ func NewProducer(cfg RabbitCfg, queueName string) (*Producer, Finalizer) {
 	if err != nil {
 		log.Fatal("error getting channel: " + err.Error())
 	}
+	err = ch.ExchangeDeclare(
+		"delayed_notifications",
+		"x-delayed-message",
+		true,
+		false,
+		false,
+		false,
+		amqp.Table{
+			"x-delayed-type": "direct",
+		},
+	)
+	if err != nil {
+		log.Fatal("error declaring delayed exchange: " + err.Error())
+	}
 	q, err := ch.QueueDeclare(queueName, true, false, false, false, nil)
 	if err != nil {
 		log.Fatal("error declaring queue: " + err.Error())
+	}
+	err = ch.QueueBind(q.Name, "delayed_routing", "delayed_notifications", false, nil)
+	if err != nil {
+		log.Fatal("error binding queue: " + err.Error())
 	}
 	return &Producer{
 			mqComponents: struct {
@@ -54,13 +74,27 @@ func NewProducer(cfg RabbitCfg, queueName string) (*Producer, Finalizer) {
 }
 
 func (p *Producer) ProduceWithJSON(jsonMsg []byte) error {
+
+	var msg models.Notification
+	var delayed bool
+	err := sonic.ConfigDefault.Unmarshal(jsonMsg, &msg)
+	if err == nil {
+		delayed = msg.Delayed
+	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
-	err := p.mqComponents.ch.PublishWithContext(ctx, "", p.mqComponents.q.Name, false, false, amqp.Publishing{
+	publishing := amqp.Publishing{
 		ContentType:  "application/json",
 		Body:         jsonMsg,
 		DeliveryMode: amqp.Persistent,
-	})
+	}
+	if delayed {
+		delay := time.Until(msg.Deadline)
+		publishing.Headers = amqp.Table{
+			"x-delay": delay,
+		}
+	}
+	err = p.mqComponents.ch.PublishWithContext(ctx, "", p.mqComponents.q.Name, false, false, publishing)
 	if err != nil {
 		return errors.New("error producing json msg: " + err.Error())
 	}
